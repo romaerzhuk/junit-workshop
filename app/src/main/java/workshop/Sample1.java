d6d6afc994d41848b9e7f9d98ab5bfd1e65a117a
Roman Erzhukov <ergukov@i-teco.ru>
2016-03-27 11:26:59 +0300

    testParse_NullPointerException: OK

--- app/src/main/java/workshop/Sample1.java
+++ app/src/main/java/workshop/Sample1.java
@@ -1,7 +1,5 @@
 package workshop;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import org.apache.commons.lang3.time.DateUtils;
 
@@ -9,7 +7,7 @@ public class Sample1 {
   public Date parse(String dateTime) throws IllegalArgumentException {
     try {
       return DateUtils.parseDate(dateTime, "yyyy-MM-dd'T'hh:mm:ss");
-    } catch (ParseException e) {
+    } catch (Exception e) {
       throw new IllegalArgumentException("Некорректно: dateTime=[" + dateTime + "]");
     }
   }
