c9a24443e3d41593ebdce7ac7c94b64b789794ef
Roman Erzhukov <ergukov@i-teco.ru>
2016-03-27 11:22:24 +0300

    testParse_Exception: OK

--- app/src/main/java/workshop/Sample1.java
+++ app/src/main/java/workshop/Sample1.java
@@ -3,13 +3,14 @@ package workshop;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.apache.commons.lang3.time.DateUtils;
 
 public class Sample1 {
   public Date parse(String dateTime) throws IllegalArgumentException {
     try {
-      return new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss").parse(dateTime);
+      return DateUtils.parseDate(dateTime, "yyyy-MM-dd'T'hh:mm:ss");
     } catch (ParseException e) {
-      throw new UnsupportedOperationException();
+      throw new IllegalArgumentException("Некорректно: dateTime=[" + dateTime + "]");
     }
   }
 }
