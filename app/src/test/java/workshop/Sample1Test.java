3264d133cc9cbb87d3d0ba96d4084fa0802ace3e
Roman Erzhukov <ergukov@i-teco.ru>
2016-03-27 11:18:55 +0300

    добавлен testParse_Exception: fail

--- app/src/test/java/workshop/Sample1Test.java
+++ app/src/test/java/workshop/Sample1Test.java
@@ -1,10 +1,13 @@
 package workshop;
 
 import java.util.Date;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 import static ru.iteco.test.utils.TestUtil.toDate;
 
 /**
@@ -23,5 +26,15 @@ public class Sample1Test {
     Date expected = toDate("27.03.2016 10:33:12");
 
     assertEquals(expected, subj.parse("2016-03-27T10:33:12"));
+  }
+
+  @Test
+  public void testParse_Exception() throws Exception {
+    try {
+      subj.parse("2016-03-27T10:33:1?");
+      fail();
+    } catch (IllegalArgumentException e) {
+      assertEquals("Некорректно: dateTime=[2016-03-27T10:33:1?]", e.getMessage());
+    }
   }
 }
