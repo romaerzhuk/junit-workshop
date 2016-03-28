006107a39298d0f2a389598f2239c70bc43d8402
Roman Erzhukov <ergukov@i-teco.ru>
2016-03-28 09:57:58 +0300

    refactoring: testSaveToFile & testSaveToFile_cursor_is_empty используют cursorTemplate: OK

--- app/src/main/java/workshop/Sample2.java
+++ app/src/main/java/workshop/Sample2.java
@@ -6,20 +6,21 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.sql.SQLException;
+import java.util.Iterator;
 import javax.annotation.Resource;
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Sample2 {
-  private Logger log = LoggerFactory.getLogger(getClass());
-
   @Resource
   private Dao dao;
   @Resource
   File dir;
   @Resource
   private Formatter formatter;
+  @Resource
+  private CursorTemplate cursorTemplate;
 
   /**
    * Сохраняет в файл name.txt список счетов удовлетворяющих условию
@@ -30,30 +31,27 @@ public class Sample2 {
    * @throws SQLException
    */
   public boolean saveToFile(String name, Predicate predicate) throws IOException, SQLException {
-    Writer out = null;
-    Cursor<Account> cursor = dao.openByName(name);
-    try {
-      while (cursor.hasNext()) {
-        Account account = cursor.next();
-        if (predicate.apply(account)) {
-          if (out == null) {
-            out = new OutputStreamWriter(new FileOutputStream(new File(dir, name + ".txt")), "UTF-8");
+    return cursorTemplate.execute(dao.cursorCreatorByName(name), new CursorCallback<Account, Boolean>() {
+        @Override
+        public Boolean doWithCursor(Iterator<Account> cursor) throws SQLException {
+          Writer out = null;
+          try {
+            while (cursor.hasNext()) {
+              Account account = cursor.next();
+              if (predicate.apply(account)) {
+                if (out == null) {
+                  out = new OutputStreamWriter(new FileOutputStream(new File(dir, name + ".txt")), "UTF-8");
+                }
+                formatter.write(out, account);
+              }
+            }
+            return out != null;
+          } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e); // TODO поправить хак
+          } finally {
+            IOUtils.closeQuietly(out);
           }
-          formatter.write(out, account);
         }
-      }
-      return out != null;
-    } finally {
-      close(cursor);
-      IOUtils.closeQuietly(out);
-    }
-  }
-
-  private void close(Cursor<Account> cursor) {
-    try {
-      cursor.close();
-    } catch (Throwable t) {
-      log.warn("Невозможно закрыть курсор", t);
-    }
+      });
   }
 }
