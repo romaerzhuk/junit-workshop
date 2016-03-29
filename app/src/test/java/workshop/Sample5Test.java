a97fc79af205e5a034cf4b12fa9e1193134d453d
Roman Erzhukov <ergukov@i-teco.ru>
2016-03-29 09:41:33 +0300

    добавлен параметризированный тест

--- app/src/test/java/workshop/Sample5Test.java
+++ app/src/test/java/workshop/Sample5Test.java
@@ -1,5 +1,6 @@
 package workshop;
 
+import static org.mockito.Mockito.times;
 import static ru.iteco.test.utils.MockUtils.verifyInOrder;
 import static ru.iteco.test.utils.TestUtil.uid;
 
@@ -9,6 +10,9 @@ import org.junit.rules.TestRule;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import ru.iteco.test.utils.annotations.BeforeMock;
+import ru.iteco.test.utils.annotations.Parameterized;
+import ru.iteco.test.utils.annotations.ParameterizedField;
+import ru.iteco.test.utils.rules.ParameterizedTest;
 
 public class Sample5Test {
   @Rule
@@ -19,6 +23,7 @@ public class Sample5Test {
 
   @Mock
   private Dao dao;
+  private State state;
 
   @BeforeMock
   public void init() {
@@ -53,4 +58,28 @@ public class Sample5Test {
 
     verifyInOrder(dao).update(pos);
   }
+
+  @Test
+  @Parameterized("setState") // указывает на метод(ы), которыми параметризируется тест
+  public void testSave() {
+    Position pos = new Position();
+    pos.setState(state);
+
+    boolean init = state == null || state == State.INIT;
+
+    subj.save(pos);
+
+    verifyInOrder(dao, times(init ? 1: 0)).insert(pos);
+    verifyInOrder(dao, times(init ? 0: 1)).update(pos);
+  }
+
+  @ParameterizedField("state") // сбрасывает state=null после теста
+  private void setState(ParameterizedTest test) {
+    for (State s: State.values()) {
+      test.run(state);
+    }
+    state = null;
+    test.run(state);
+  }
+
 }
