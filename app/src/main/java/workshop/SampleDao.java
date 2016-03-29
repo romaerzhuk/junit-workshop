3deb5909555fc1d12f9d88bb416e6df5b40a1d94
Roman Erzhukov <ergukov@i-teco.ru>
2016-03-29 07:17:19 +0300

    testFindByCriteria: OK

--- app/src/main/java/workshop/SampleDao.java
+++ app/src/main/java/workshop/SampleDao.java
@@ -7,6 +7,14 @@ import workshop.dao.SqlQuery;
 public class SampleDao extends AbstractDao {
 
   public List<Account> findByCriteria(Account criteria) {
-    return null; // TODO
+    SqlQuery query = new SqlQuery("SELECT ID, NAME, AMOUNT, CLOSED"
+        + "\nFROM ACCOUNT");
+    query.add("NAME", criteria.getName());
+    query.add("AMOUNT", criteria.getAmount());
+    return self().executeQuery(Account.class, query);
+  }
+
+  SampleDao self() {
+    return this;
   }
 }
